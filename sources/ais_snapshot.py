67737d94b5356303ac9ca39574dafd8afa2f9e43
JahziaCCC <s300s700@gmail.com>
2026-07-22 12:40:47 +0300

    Create ais_snapshot.py

---
+++ sources/ais_snapshot.py
@@ -0,0 +1,17 @@
+# AIS Snapshot Module
+# Strategic Maritime Early Warning System
+
+
+def get_vessel_data(chokepoint):
+
+    # بيانات تجريبية مؤقتة
+    # سيتم استبدالها لاحقاً بمصدر AIS حقيقي
+
+    data = {
+        "vessel_count": 120,
+        "oil_tankers": 35,
+        "stopped_vessels": 5,
+        "abnormal_movements": 2
+    }
+
+    return data
