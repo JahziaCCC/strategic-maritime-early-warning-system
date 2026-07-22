be718fdfa02ec3eeabfa44e5370e26d21125da6d
JahziaCCC <s300s700@gmail.com>
2026-07-22 14:27:03 +0300

    Update vessel_tracker.py

--- sources/vessel_tracker.py
+++ sources/vessel_tracker.py
@@ -3,69 +3,77 @@ Vessel Intelligence Tracker
 Strategic Maritime Early Warning System
 """
 
-
+from sources.ais_live_provider import get_live_vessels
 from intelligence.geo_engine import locate_vessel
+
 
 
 def get_tracked_vessels():
 
-    vessels = [
 
-        # Strait of Hormuz
-        {
-            "name": "Tanker-001",
-            "type": "Oil Tanker",
-            "lat": 26.2,
-            "lon": 56.1
-        },
+    # محاولة جلب بيانات AIS الحقيقية
 
-        {
-            "name": "Tanker-002",
-            "type": "Oil Tanker",
-            "lat": 26.1,
-            "lon": 56.0
-        },
-
-        {
-            "name": "Cargo-001",
-            "type": "Cargo Ship",
-            "lat": 26.3,
-            "lon": 56.2
-        },
+    vessels = get_live_vessels()
 
 
-        # Bab el Mandeb
-        {
-            "name": "Cargo-002",
-            "type": "Cargo Ship",
-            "lat": 12.8,
-            "lon": 43.2
-        },
+    # في حال عدم توفر AIS نستخدم البيانات التجريبية
 
-        {
-            "name": "Tanker-003",
-            "type": "Oil Tanker",
-            "lat": 12.7,
-            "lon": 43.3
-        },
+    if not vessels:
+
+        vessels = [
+
+            {
+                "name": "Tanker-001",
+                "type": "Oil Tanker",
+                "lat": 26.2,
+                "lon": 56.1
+            },
+
+            {
+                "name": "Tanker-002",
+                "type": "Oil Tanker",
+                "lat": 26.1,
+                "lon": 56.0
+            },
+
+            {
+                "name": "Cargo-001",
+                "type": "Cargo Ship",
+                "lat": 26.3,
+                "lon": 56.2
+            },
 
 
-        # Suez Canal
-        {
-            "name": "Container-001",
-            "type": "Container Ship",
-            "lat": 30.5,
-            "lon": 32.5
-        },
+            {
+                "name": "Cargo-002",
+                "type": "Cargo Ship",
+                "lat": 12.8,
+                "lon": 43.2
+            },
 
-        {
-            "name": "Tanker-004",
-            "type": "Oil Tanker",
-            "lat": 30.6,
-            "lon": 32.6
-        }
+            {
+                "name": "Tanker-003",
+                "type": "Oil Tanker",
+                "lat": 12.7,
+                "lon": 43.3
+            },
 
-    ]
+
+            {
+                "name": "Container-001",
+                "type": "Container Ship",
+                "lat": 30.5,
+                "lon": 32.5
+            },
+
+            {
+                "name": "Tanker-004",
+                "type": "Oil Tanker",
+                "lat": 30.6,
+                "lon": 32.6
+            }
+
+        ]
 
 
     results = []
@@ -73,12 +81,15 @@ def get_tracked_vessels():
 
     for vessel in vessels:
 
+
         location = locate_vessel(
             vessel["lat"],
             vessel["lon"]
         )
 
+
         vessel["zone"] = location["arabic_name"]
+
 
         results.append(vessel)
 
